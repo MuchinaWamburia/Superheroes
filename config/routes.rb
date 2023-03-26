2b9d017e1970b2bc0a649bd5282fc92952475189
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-26 14:27:12 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  resources :heros, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update] 
+  resources :hero_powers, only: [:create]
+  get 'contact', to: 'contact#index'
+  post 'contact', to: 'contact#create'
+  end
